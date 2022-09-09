3b24674b0ded2f4cd192bbcb0ac32c4d018b7371
Dan2TDMJace <112831108+Dan2TDMJace@users.noreply.github.com>
2022-09-08 20:23:42 -0500

    Rebrand & Improvements: Release 1.2
    Release 1.2!
    New features:
    Integrated IE
    URL selection on startup
    Rebrand to DTJ Internet Explorer

---
+++ DTJ Internet Explorer/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim URL As String = InputBox("Enter URL", "URL")
+        WebBrowser1.Navigate(URL)
+    End Sub
+End Class
