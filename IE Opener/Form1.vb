eb024f399d78fb2d50504010088df8b34aa9b789
Dan2TDMJace <kevinhensley1944@gmail.com>
2023-01-31 19:04:31 -0600

    Update 1.4
    Back to IE Opener!
    
    -Actually opens IE

---
+++ IE Opener/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim TheBrowser As Object = CreateObject("InternetExplorer.Application")
+        TheBrowser.Visible = True
+        TheBrowser.Navigate("https://dantdmjace.neocities.org/ie-opener")
+
+    End Sub
+End Class
